e8c8e6b69d224c38a861034d73a2bb56c0153f76
Bert Palm <bert.palm@ufz.de>
2022-04-06 15:21:20 +0200

    - cleanup index.html - improved status handling with an event stream - introduced logging - never stop camera thread

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import logging
 import threading
 import time
 from importlib import import_module
@@ -14,63 +15,65 @@ from camera_opencv import Camera
 # from camera_pi import Camera
 
 app = Flask(__name__)
+status_event = threading.Event()
+status = "init"
 
-status = 'init'
-default_frame = cv2.imencode('.jpg', np.zeros((100, 100)))[1].tobytes()
 
-
-@app.route('/')
+@app.route("/")
 def index():
     """Video streaming home page."""
-    return render_template('index.html', initial_status=status)
+    return render_template("index.html", initial_status=status)
 
 
-def gen(camera):
+@app.route("/buttons", methods=["POST"])
+def handle_buttons():
+    global status
+    last = status
+    action = request.data.decode()
+    if action == "start":
+        status = "running"
+    if action == "stop":
+        status = "stopped"
+    if last == status:
+        logging.debug(f"{action=}, {status=}, => no update")
+    else:
+        logging.info(f"{action=} => {status=}")
+        status_event.set()
+    return ""
+
+
+def frame_stream(camera):
     """Video streaming generator function."""
-    yield b'--frame\r\n'
+    yield b"--frame\r\n"
     while True:
-        if status == 'stopped':
-            time.sleep(1)
-            frame = default_frame
-        else:
-            frame = camera.get_frame()
-        yield b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n--frame\r\n'
+        if status == "stopped":
+            status_event.wait()
+            continue
+        frame = camera.get_frame()
+        yield b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n--frame\r\n"
 
 
-@app.route('/video_feed')
+def status_stream():
+    while True:
+        status_event.wait()
+        status_event.clear()
+        yield f"data: {status}\n\n"
+
+
+@app.route("/video_feed")
 def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(
-        gen(Camera()),
-        mimetype='multipart/x-mixed-replace; boundary=frame'
+        frame_stream(Camera()), mimetype="multipart/x-mixed-replace; boundary=frame"
     )
 
 
-@app.route('/buttons', methods=['POST'])
-def handle_buttons():
-    global status
-    action = request.data.decode()
-    if action == 'start':
-        status = 'running'
-    if action == 'stop':
-        status = 'stopped'
-    print(f"{action=} -> {status}")
-    return ''
+@app.route("/status")
+def status_feed():
+    return Response(status_stream(), mimetype="text/event-stream")
 
 
-@app.route('/status')
-def status_stream():
-    def eventStream():
-        last = status
-        while True:
-            if last == status:
-                time.sleep(0.1)
-                continue
-            last = status
-            yield f'data: {status}\n\n'
-
-    return Response(eventStream(), mimetype="text/event-stream")
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True, debug=True)
+if __name__ == "__main__":
+    print("woooo")
+    logging.basicConfig(level="DEBUG")
+    app.run(host="0.0.0.0", threaded=True, debug=True)
